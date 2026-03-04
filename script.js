5b772bacf6b0cc300241a25450026ae4e37255af
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-03-04 21:16:47 +0100

    Make the getHumanChoice function and some playRound
    I managed to get the humanchoice function to get to working.
    Had some trouble with the parameters and the let method.

--- script.js
+++ script.js
@@ -14,6 +14,44 @@ function getComputerChoice() {
     return computerChoice
 }
 
-console.log(getComputerChoice())
+
+// Make the getHumanChoice function
+/*
+Ask the user for an input.
+Validate if the given input is valid - it must be either rock, paper or scizors.
+If the value is invalid prompt the user again.
 
 
+*/
+function getHumanChoice() {
+    let humanChoice = prompt("Pick rock, paper or scizors: ");
+    return humanChoice;
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+let humanChoice = getHumanChoice().toLowerCase();
+let computerChoice = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    
+    // evaluate when the human will get a score and when the computer
+    if (humanChoice == "rock" & computerChoice == "scizors"
+        || humanChoice == "paper" & computerChoice == "rock"
+        || humanChoice == "scizors" & computerChoice == "paper") {
+            humanScore += 1;
+        }
+    else if (humanChoice == "rock" & computerChoice == "paper"
+        || humanChoice == "paper" & computerChoice == "scizors"
+        || humanChoice == "scizors" & computerChoice == "rock") {
+            computerScore += 1;
+        }
+   
+    return humanChoice, humanScore
+
+
+}
+console.log(getComputerChoice());
+console.log(playRound(humanChoice, computerChoice))
